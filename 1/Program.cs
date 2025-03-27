1279c677dcfd81e311afc90220e70ed72a62ca47
RomanDuvakin <duvakin.r@GMAIL.COM>
2025-03-27 15:07:43 +0200

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        var cars = new List<Car> { new Car { Type = "Coupe", Year = 2022 }, new Car { Type = "Truck", Year = 2020 }, new Car { Type = "Coupe", Year = 2021 }, new Car { Type = "Bus", Year = 2019 }, new Car { Type = "Truck", Year = 2023 } };
+        var sortedCars = cars.OrderBy(car => car.Type)           .ThenBy(car => car.Year); 
+
+    foreach (var car in sortedCars) { Console.WriteLine($"{car.Type} - {car.Year}"); }
+    }
+}
+class Car { public string Type { get; set; } public int Year { get; set; } }
